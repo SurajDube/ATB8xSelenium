32f38674110cc5580e81d15ead1f403ae5140b88
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2025-01-14 00:15:50 +0530

    commit

--- src/test/java/com/thetestingacademy/ex01_18122024/Selenium01.java
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium01.java
@@ -1,4 +1,17 @@
-package com.thetestingacademy.ex01_18122024;
+package com.TheTestingAcademy.ex01_18122024;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
 
 public class Selenium01 {
+    @Test
+    public void test_Selenium(){
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.flipkart.com/");
+
+
+    }
 }
